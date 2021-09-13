7543a0a309cf926e1c6873b3d93605851d954f1f
Over <averinkit@gmail.com>
2021-09-13 19:59:38 +0400

    Добавил комментарии

--- hh.py
+++ hh.py
@@ -8,18 +8,19 @@ array_list = []
 
 def func(num):
     for i in range (num):
-        size = randint(1,q)
-        while size in array_sizes[:]:
-            size = randint(1,q)
+        size = randint(1,q)                                 #генерируется размер массивов
+        while size in array_sizes[:]:                       #если массив такого размера существует,
+            size = randint(1,q)                             #то генерируется новый размер
         array_sizes.append(size)
         
-        array = [randint(p,q) for _ in range(size)]
-        array.sort()
+        array = [randint(p,q) for _ in range(size)]         #массив заполняется случайными числами
+        array.sort()                                        #и сортируется по возрастанию
         
-        if (i+1)%2:
+        if (i+1)%2:                                         
             array_list.append(array)
         else:
-            array_list.append(array[::-1])
+            array_list.append(array[::-1])                  #если номер массива нечётный, то его элементы записываются в обратном порядке
             
 func(n)
 print(array_list)
+input()
